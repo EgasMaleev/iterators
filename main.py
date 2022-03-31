5f22ffd9b24cd5b3554a342d73d73c76253cad81
Egor Maleev <e9126515199@icloud.com>
2022-03-31 12:34:04 +0500

    iter and gen

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-class MyIterator:
-    def __init__(self, listik):
-        self.listik = listik
-        self.count_main = 0
-        self.count_list = -1
-    def __iter__(self):
-        return(self)
-    def __next__(self):
-        self.count_list += 1
-        if self.count_list == len(self.listik[self.count_main]):
-            self.count_list = 0
-            self.count_main += 1
-        if self.count_main == len(self.listik):
-            raise StopIteration
-        return(self.listik[self.count_main][self.count_list])
+def MyGenerator(listik):
+    for item in listik:
+        for el in item:
+            yield el
+
 nested_list = [
-	['a', 'b', 'c'],
-	['d', 'e', 'f', 'h', False],
-	[1, 2, None],
-]
-iterator = MyIterator(nested_list)
-for item in iterator:
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None],
+    ]
+for item in MyGenerator(nested_list):
     print(item)
-flat_list = [item for item in MyIterator(nested_list)]
+flat_list = [item for item in MyGenerator(nested_list)]
 print(flat_list)
